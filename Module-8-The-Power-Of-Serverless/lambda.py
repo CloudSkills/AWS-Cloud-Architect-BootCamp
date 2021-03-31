d66508a64f5498363e228fe36985912b091e1bf0
Michael Levan <mlevan1992@gmail.com>
2021-03-31 08:52:02 -0400

    lambda boto3

--- Module-8-The-Power-Of-Serverless/lambda.py
+++ Module-8-The-Power-Of-Serverless/lambda.py
@@ -1,28 +1,35 @@
+# Example way to run the script with a Lambda Function name and ARN:
+# python .\lambda.py hellpythonlambda arn:aws:iam::912101370089:role/service-role/cloudskillslambdarole
+
 import boto3
 import botocore
 import sys
 import logging
 
-def create_lambda(name):
+
+def create_lambda(name, role):
     try:
         client = boto3.client('lambda')
+
+        filePath = "./pythonscript.zip"
 
         client.create_function(
             FunctionName=name,
             Runtime='python3.8',
-            Role="",
+            Role=role,
+            Handler='lambda_handler',
+            Code={'ZipFile': open(filePath, 'rb').read(), },
         )
     
     except botocore.exceptions.ClientError as error:
         raise error
-
-    except botocore.exceptions.ResourceConflictExcept as conflict:
-        print(conflict)
-
+    
     except botocore.exceptions.ParamValidationError as e:
-        logging.exception('Parameter Validation: %s' & e)
+        logging.exception(e)
+
 
 name = sys.argv[1]
+role = sys.argv[2]
 
 if __name__ == '__main__':
-    create_lambda(name)
+    create_lambda(name, role)
